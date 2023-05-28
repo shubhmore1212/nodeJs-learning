7e25da08626387f331ee9c1dde10e3cfd4d9fbbe
Shubham More <shubham.more@joshsoftware.com>
2023-05-28 23:05:05 +0530

    Added cors whitelist url and initializing db

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+
 import greetRoutes from "./routes/public/greet.js";
 import userRoutes from "./routes/public/user.js";
+import { sequelize } from "./utils/db.js";
 
 dotenv.config();
 const app = express();
 
-app.use(cors());
+const WHITELIST = ["http://localhost:9000", "http://localhost:8000"];
+
+app.use(
+  cors({
+    origin: WHITELIST,
+  })
+);
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
 
 // Taking arguments from command line
 const args = process.argv.splice(2);
@@ -18,6 +28,14 @@ const PORT = args[0]?.split("=")[1] || process.env.PORT || 8000;
 app.use("/greet", greetRoutes);
 app.use("/user", userRoutes);
 
-app.listen(PORT, () => {
-  console.log(`listening to port ${PORT}`);
-});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connection has been established successfully");
+    app.listen(PORT, () => {
+      console.log(`Listening to port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Database connection: ${error}`);
+  });
