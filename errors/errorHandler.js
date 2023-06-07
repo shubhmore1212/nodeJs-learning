e267fd9afa1f5c329cb5f1a3a704ef8619109551
Shubham More <shubham.more@joshsoftware.com>
2023-06-07 17:39:18 +0530

    Added error handling fpor validation

--- errors/errorHandler.js
+++ errors/errorHandler.js
@@ -2,6 +2,12 @@ import { response } from "../utils/responseUtil.js";
 
 export const errorHandler = (error, req, res, next) => {
   console.log("Error", error);
+  if (error.name === "ValidationError") {
+    return response(res, 422, {
+      type: "Validation Error",
+      details: error.details,
+    });
+  }
   if (error instanceof Error) {
     return response(res, error.statusCode, error.message);
   }
--- services/auth.js
+++ services/auth.js
@@ -2,15 +2,16 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 import { User } from "../models/user.js";
+import AppError from "../errors/appError.js";
 
 export const loginUser = async (payload) => {
   const { email, password } = payload;
 
   const user = await User.findOne({ where: { email } });
-  if (!user) return res.status(400).json({ msg: "User does not exist" });
+  if (!user) throw new AppError("User does not exist", 400);
 
   const isMatch = await bcrypt.compare(password, user.dataValues.password);
-  if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials" });
+  if (!isMatch) throw new AppError("Invalid Credentials", 400);
 
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
     expiresIn: "1d",
